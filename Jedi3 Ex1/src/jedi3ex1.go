6a379a10f8ba871dec52bc5759e337bc6b62222b
yuribenelli <y.benelli@mind-informatica.com>
2022-03-29 16:19:38 +0200

    Update jedi3ex1.go

--- Jedi3 Ex1/src/jedi3ex1.go
+++ Jedi3 Ex1/src/jedi3ex1.go
@@ -3,9 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	count := 10000
+	//Using two methods for iteration
+
+	count := 100
 	for i := 0; i < count; i++ {
 		fmt.Printf("%v\n", i)
+	}
+
+	//single condition
+	i := 0
+	for i < 100 {
+		fmt.Printf("%v\n", i)
+		i++
+
 	}
 
 }
