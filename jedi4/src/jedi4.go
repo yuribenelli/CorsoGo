10f06fe81691f16c8f4e17a9662ebccbbb3337f0
yuribenelli <y.benelli@mind-informatica.com>
2022-03-30 14:02:36 +0200

    jedi4

--- jedi4/src/jedi4.go
+++ jedi4/src/jedi4.go
@@ -1,31 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	// stampo la lista dei numeri primi da 0 a n
-START:
+	var userNumber int
 	fmt.Println("inserisci il numero")
-	var n int
-	fmt.Scan(&n)
-	if n <= 1 {
-		fmt.Println("il numero deve essere maggiore di 1")
-		goto START
-	}
-	var isPrime bool = true
-	for i := n; i >= 2; i-- {
-		for j := n - 1; j >= 2; j-- {
-			if n%j == 0 {
-				isPrime = false
-			}
+	fmt.Scan(&userNumber)
+	if userNumber > 1 {
 
-		}
-		fmt.Println(i)
-		if isPrime {
-			x := []int{}
-			x = append(x, i)
-		}
-
+	} else {
+		fmt.Println("")
 	}
-	fmt.Println(isPrime)
+
 }
